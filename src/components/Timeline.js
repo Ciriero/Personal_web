0116863de3f7133d3f01b9ea528251dacade672d
Christian Calle C <chcallec@gmail.com>
2023-01-21 15:46:07 +0100

    added timeline.js

---
+++ src/components/Timeline.js
@@ -0,0 +1,108 @@
+import React from "react";
+import { WrapperTimeline } from "../styles/Timeline.styles";
+
+const Timeline = () => {
+  return (
+    <WrapperTimeline>
+      <div className="timeline-title">
+        <h2>Timeline</h2>
+        <div className="underline udl"></div>
+      </div>
+      <div className="timelines-items">
+        <div className="timelines-item">
+          <div className="timeline-dot"></div>
+          <div className="timeline-date">2015</div>
+          <div className="timeline-content">
+            <h3>Timeline Title</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+              doloremque molestiae maiores a soluta consequatur fugiat illo
+              reiciendis eligendi dicta alias voluptas illum modi, aspernatur
+              quos, aperiam necessitatibus nulla error!
+            </p>
+          </div>
+        </div>
+        <div className="timelines-item">
+          <div className="timeline-dot"></div>
+          <div className="timeline-date">2016</div>
+          <div className="timeline-content">
+            <h3>Timeline Title</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+              doloremque molestiae maiores a soluta consequatur fugiat illo
+              reiciendis eligendi dicta alias voluptas illum modi, aspernatur
+              quos, aperiam necessitatibus nulla error!
+            </p>
+          </div>
+        </div>
+        <div className="timelines-item">
+          <div className="timeline-dot"></div>
+          <div className="timeline-date">2017</div>
+          <div className="timeline-content">
+            <h3>Timeline Title</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+              doloremque molestiae maiores a soluta consequatur fugiat illo
+              reiciendis eligendi dicta alias voluptas illum modi, aspernatur
+              quos, aperiam necessitatibus nulla error!
+            </p>
+          </div>
+        </div>
+        <div className="timelines-item">
+          <div className="timeline-dot"></div>
+          <div className="timeline-date">2018</div>
+          <div className="timeline-content">
+            <h3>Timeline Title</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+              doloremque molestiae maiores a soluta consequatur fugiat illo
+              reiciendis eligendi dicta alias voluptas illum modi, aspernatur
+              quos, aperiam necessitatibus nulla error!
+            </p>
+          </div>
+        </div>
+        <div className="timelines-item">
+          <div className="timeline-dot"></div>
+          <div className="timeline-date">2019</div>
+          <div className="timeline-content">
+            <h3>Timeline Title</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+              doloremque molestiae maiores a soluta consequatur fugiat illo
+              reiciendis eligendi dicta alias voluptas illum modi, aspernatur
+              quos, aperiam necessitatibus nulla error!
+            </p>
+          </div>
+        </div>
+        <div className="timelines-item">
+          <div className="timeline-dot"></div>
+          <div className="timeline-date">2020</div>
+          <div className="timeline-content">
+            <h3>Timeline Title</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+              doloremque molestiae maiores a soluta consequatur fugiat illo
+              reiciendis eligendi dicta alias voluptas illum modi, aspernatur
+              quos, aperiam necessitatibus nulla error!
+            </p>
+          </div>
+        </div>
+        <div className="timelines-item">
+          <div className="timeline-dot"></div>
+          <div className="timeline-date">2021</div>
+          <div className="timeline-content">
+            <h3>Timeline Title</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+              doloremque molestiae maiores a soluta consequatur fugiat illo
+              reiciendis eligendi dicta alias voluptas illum modi, aspernatur
+              quos, aperiam necessitatibus nulla error!
+            </p>
+          </div>
+        </div>
+      </div>
+    </WrapperTimeline>
+  );
+};
+
+export default Timeline;
---
+++ src/styles/Timeline.styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const WrapperTimeline = styled.section`
+  background-color: var(--clr-primary-9);
+  padding: 6rem 1rem;
+  .timeline-title {
+    text-align: center;
+    margin-bottom: 5rem;
+    .udl {
+      margin: 1rem auto;
+    }
+  }
+`;
