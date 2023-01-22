7ce882513f651123881772c18e5aa3e67190af70
Christian Calle C <chcallec@gmail.com>
2023-01-22 15:51:21 +0100

    added provisional data

--- src/components/Proyects.js
+++ src/components/Proyects.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+import { WrapperProyects, WrapperCenter } from "../styles/Proyects.styles";
+import { projects } from "../data/proyects";
+import { logos } from "../data/logos";
+import ProjectsItems from "./ProjectsItems";
+import LogoItem from "./LogoItem";
 
 const Proyects = () => {
+  const [active, setActive] = useState("pro");
   return (
-    <div>Proyects</div>
-  )
-}
+    <WrapperProyects>
+      <div className="title-portfolio">
+        <h1>Portfolio</h1>
+        <div className="underline udl"></div>
+        <p className="intro-proyect">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, nisi
+          obcaecati veritatis voluptates debitis nostrum vero placeat provident
+          dolore quo!
+        </p>
+        <div className="buttons">
+          <button
+            className={active === "tec" ? "btn-proy" : "claseb"}
+            onClick={() => setActive("pro")}
+            active={`${active} === "pro`}
+          >
+            Proyectos
+          </button>
+          <button
+            className={active === "pro" ? "btn-proy" : "claseb"}
+            onClick={() => setActive("tec")}
+            active={`${active} === "tec`}
+          >
+            Tecnologías
+          </button>
+        </div>
+      </div>
 
-export default Proyects
+      <WrapperCenter>
+        <>
+          {active === "pro" &&
+            projects.map((item) => <ProjectsItems key={item.id} item={item} />)}
+          {active === "tec" &&
+            logos.map((logo) => <LogoItem key={logo.id} logo={logo} />)}
+        </>
+      </WrapperCenter>
+    </WrapperProyects>
+  );
+};
+
+export default Proyects;
---
+++ src/data/logos.js
@@ -0,0 +1,13 @@
+export const logos = [
+    {id: 10, name: "html", img: "../../utils/html.svg"},
+    {id: 20, name: "css", img: "../../utils/css.svg"},
+    {id: 30, name: "sass", img: "../../utils/sass.svg"},
+    {id: 40, name: "styled-components", img: "../../utils/styled.svg"},
+    {id: 50, name: "javascript", img: "../../utils/js.svg"},
+    {id: 60, name: "react", img: "../../utils/react.svg"},
+    {id: 70, name: "git", img: "../../utils/git.svg"},
+    {id: 80, name: "nodejs", img: "../../utils/nodejs.svg"},
+    {id: 90, name: "mongoDB", img: "../../utils/mongodb.svg"},
+]
+
+//Icons from https://iconduck.com/
