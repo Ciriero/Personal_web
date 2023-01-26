7b4ff49b30ea1e4a3fa250a9dc6220c7d5355a55
Christian Calle C <chcallec@gmail.com>
2023-01-26 19:22:41 +0100

    added first colors

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Proyects from "./components/Proyects";
 import Timeline from "./components/Timeline";
+import Theme from "./colorTheme";
+import { ThemeProvider } from "styled-components";
+import { useState } from "react";
 
 function App() {
+  const [theme, setTheme] = useState("lg")
   return (
-    <>
+    <ThemeProvider theme={Theme[theme]}>
       <Navbar />
-      <Hero />
+      <Hero theme={theme} setTheme={setTheme}/>
       <About />
       <Timeline />
       <Proyects />
       <Form />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/colorTheme.js
@@ -0,0 +1,16 @@
+const darkTheme = {
+    bodyHero: "#121212",
+    colorHero: "#BB86FC"
+}
+
+const lgTheme = {
+    bodyHero: "var(--clr-primary-9)",
+    colorHero: "var(--clr-primary-2)"
+}
+
+const Theme = {
+    dark: darkTheme,
+    lg: lgTheme
+}
+
+export default Theme
